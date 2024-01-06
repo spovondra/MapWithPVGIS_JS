dd087bfe5b413437a759a4f016983cc4f9ed6044
spovondra <spovondra@gmail.com>
2024-01-06 21:41:26 +0100

    oprava bugů

--- public/main.js
+++ public/main.js
@@ -1,131 +1,149 @@
 let map;
 
 $(document).ready(function () {
+    // Výchozí hodnoty pro zeměpisnou šířku a délku
     const defaultLatitude = 49.744;
     const defaultLongitude = 15.339;
-    initMap(defaultLatitude, defaultLongitude);
+    initializeMap(defaultLatitude, defaultLongitude);
 
+    // Nastavení akce po kliknutí na mapu
     map.on('click', function (e) {
-        const clickedLatitude = e.latlng.lat.toFixed(3);
-        const clickedLongitude = e.latlng.lng.toFixed(3);
+        const clickedLatitude = e.latlng.lat;
+        const clickedLongitude = e.latlng.lng;
 
-        $('#latitude').val(clickedLatitude);
-        $('#longitude').val(clickedLongitude);
+        // Aktualizace políček s hodnotami zeměpisné šířky a délky
+        $('#latitude').val(clickedLatitude.toFixed(3));
+        $('#longitude').val(clickedLongitude.toFixed(3));
 
-        // Update waypoint and recalculate data
+        // Aktualizace značky na mapě a přepočet dat
         updateWaypoint(clickedLatitude, clickedLongitude);
         calculatePVGISData();
     });
 });
 
-function initMap(latitude, longitude) {
+function initializeMap(latitude, longitude) {
+    // Zaokrouhlené hodnoty zeměpisné šířky a délky pro počáteční umístění mapy
     const roundedLatitude = parseFloat(latitude).toFixed(3);
     const roundedLongitude = parseFloat(longitude).toFixed(3);
 
+    // Inicializace mapy s výchozím umístěním
     map = L.map('map').setView([roundedLatitude, roundedLongitude], 7);
 
+    // Přidání zdrojů z OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
+        attribution: '© OpenStreetMap přispěvatelé'
     }).addTo(map);
 
-    // Initial waypoint
+    // Inicializace počáteční značky
     updateWaypoint(latitude, longitude);
 }
 
-
 function updateWaypoint(latitude, longitude) {
+    // Odstranění všech značek na mapě
     map.eachLayer(function (layer) {
         if (layer instanceof L.Marker) {
             map.removeLayer(layer);
         }
     });
 
+    // Přidání nové značky na zadané souřadnice
     L.marker([latitude, longitude]).addTo(map);
 }
 
 function searchLocation() {
-    performSearch();
+    performSearch(); // Zahájení vyhledávání
 }
 
 $('#locationSearch').keyup(function (event) {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter') { // Reakce na stisk klávesy Enter při psaní do pole pro vyhledávání
         performSearch();
     }
 });
 
-
 function performSearch() {
+    // Získání hodnoty z pole pro vyhledávání
     const locationQuery = $('#locationSearch').val();
     if (locationQuery.trim() === "") {
-        alert("Please enter a location");
+        alert("Prosím zadejte místo"); // Upozornění na prázdný vstup
         return;
     }
 
+    // AJAX požadavek pro geokódování místa
     $.ajax({
         url: 'https://nominatim.openstreetmap.org/search',
         method: 'GET',
         data: { q: locationQuery, format: 'json', limit: 1 },
         success: function (response) {
             if (response.length > 0) {
+                // Získání prvního výsledku
                 const result = response[0];
-                const latitude = parseFloat(result.lat).toFixed(3);
-                const longitude = parseFloat(result.lon).toFixed(3);
+                const latitude = parseFloat(result.lat);
+                const longitude = parseFloat(result.lon);
 
-                $('#latitude').val(latitude);
-                $('#longitude').val(longitude);
+                // Aktualizace políček s novými hodnotami
+                $('#latitude').val(latitude.toFixed(3));
+                $('#longitude').val(longitude.toFixed(3));
 
-                // Update waypoint and recalculate data
+                // Aktualizace značky na mapě a přepočet dat
                 updateWaypoint(latitude, longitude);
                 calculatePVGISData();
             } else {
-                alert("Location not found");
+                // Upozornění, že místo nebylo nalezeno
+                alert("Místo nenalezeno");
             }
         },
         error: function (xhr) {
-            console.error('Error:', xhr.status);
-            alert("Error fetching location data");
+            // Zobrazení chyby při načítání dat
+            console.error('Chyba:', xhr.status);
+            alert("Chyba při načítání dat o místě");
         }
     });
 }
 
 function calculatePVGISData() {
+    // Získání hodnot pro výpočet PVGIS dat
     const latitude = $('#latitude').val();
     const longitude = $('#longitude').val();
     const angle = $('#angle').val();
     const aspect = $('#aspect').val();
 
+    // AJAX požadavek pro výpočet dat pomocí PVGIS
     $.ajax({
         url: '/api/calculatePVGISData',
         method: 'POST',
         data: { latitude, longitude, angle, aspect },
         success: function (response) {
-            $('#pvgisData').val(response);
+            $('#pvgisData').val(response); // Aktualizace políček s výsledky
         },
-        error: function (xhr) {
-            console.error('Error:', xhr.status);
+        error: function (xhr) { // Zobrazení chyby při výpočtu dat
+            console.error('Chyba:', xhr.status);
         }
     });
 }
 
 function useOptimalValues() {
+    // Získání aktuálních hodnot zeměpisné šířky a délky
     const latitude = $('#latitude').val();
     const longitude = $('#longitude').val();
 
+    // AJAX požadavek pro získání optimálních hodnot úhlu a orientace
     $.ajax({
         url: '/api/fetchOptimalValues',
         method: 'POST',
         data: { latitude, longitude },
         success: function (response) {
+            // Získání optimálních hodnot a aktualizace formuláře
             const { optimalAngle, optimalAspect } = response;
 
             $('#angle').val(optimalAngle);
             $('#aspect').val(optimalAspect);
 
+            // Aktualizace značky na mapě a přepočet dat
             updateWaypoint(latitude, longitude);
             calculatePVGISData();
         },
-        error: function (xhr) {
-            console.error('Error:', xhr.status);
+        error: function (xhr) { // Zobrazení chyby při načítání optimálních hodnot
+            console.error('Chyba:', xhr.status);
         }
     });
 }
--- server.js
+++ server.js
@@ -4,51 +4,63 @@ const bodyParser = require('body-parser');
 const https = require('https');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Zvolený port
 
+// Nastavení middleware pro zpracování URL-encoded a JSON dat (pro práci s klientem)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Nastavení statických adresářů pro Leaflet a veřejné
 app.use(express.static(path.join(__dirname, 'node_modules/leaflet/dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Konfigurace cesty pro zobrazení hlavní stránky
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// API pro výpočet PVGIS dat na základě zadaných parametrů
 app.post('/api/calculatePVGISData', async (req, res) => {
     try {
+        // Získání parametrů
         const { latitude, longitude, angle, aspect } = req.body;
         const apiUrl = `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?lat=${latitude}&lon=${longitude}&peakpower=1&loss=1&angle=${angle}&aspect=${aspect}`;
 
+        // Volání funkce pro načtení dat z externího API
         const apiRes = await fetchData(apiUrl);
         res.send(apiRes);
-    } catch (error) {
-        console.error('Error:', error.message);
-        res.status(503).send('Service Unavailable');
+    } catch (error) { // Zpracování chyby při výpočtu dat
+        console.error('Chyba:', error.message);
+        res.status(503).send('Služba nedostupná');
     }
 });
 
+// API pro získání optimálních hodnot úhlu a orientace panelů
 app.post('/api/fetchOptimalValues', async (req, res) => {
     try {
+        // Získání parametrů
         const { latitude, longitude } = req.body;
         const apiUrl = `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?lat=${latitude}&lon=${longitude}&raddatabase=PVGIS-SARAH2&userhorizon=&usehorizon=1&outputformat=json&js=1&select_database_grid=PVGIS-SARAH2&pvtechchoice=crystSi&peakpower=1.01&loss=21&mountingplace=free&optimalangles=1`;
 
+        // Volání funkce pro načtení dat z externího API
         const apiRes = await fetchData(apiUrl);
         const jsonResponse = JSON.parse(apiRes);
-        const mountingSystem = jsonResponse.inputs["mounting_system"].fixed;
 
+        // Zpracování odpovědi a získání optimálních hodnot
+        const mountingSystem = jsonResponse.inputs["mounting_system"].fixed;
         const optimalValues = {
             optimalAngle: mountingSystem.slope["optimal"] ? mountingSystem.slope.value : null,
             optimalAspect: mountingSystem.azimuth["optimal"] ? mountingSystem.azimuth.value : null
         };
 
-        res.json(optimalValues);
-    } catch (error) {
-        console.error('Error:', error.message);
-        res.status(503).json({ error: 'Service Unavailable' });
+        res.json(optimalValues); // Odeslání odpovědi v JSON formátu
+    } catch (error) { // Zpracování chyby při získávání optimálních hodnot
+        console.error('Chyba:', error.message);
+        res.status(503).json({ error: 'Služba nedostupná' });
     }
 });
 
+// Asynchronní funkce pro načtení dat z API
 async function fetchData(apiUrl) {
     return new Promise((resolve, reject) => {
         https.get(apiUrl, (apiRes) => {
@@ -67,6 +79,7 @@ async function fetchData(apiUrl) {
     });
 }
 
+// Spuštění lokálního serveru na zadaném portu (viz nahoře)
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server běží na http://localhost:${PORT}`);
 });
